a375ed857045985732bfe160841480780792fda4
Noah Glassford <noahglassford@gmail.com>
2021-02-11 12:25:57 -0500

    added physics to node files

--- projects/Project Dragon/src/InstantiatingSystem.cpp
+++ projects/Project Dragon/src/InstantiatingSystem.cpp
@@ -2,6 +2,7 @@
 #include <AssetLoader.h>
 #include <BtToGlm.h>
 #include <RenderingManager.h>
+#include <PhysicsBody.h>
 // Borrowed from https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 #pragma region String Trimming
 
@@ -85,7 +86,15 @@ void InstantiatingSystem::LoadPrefabFromFile(glm::vec3 origin, std::string filen
 		}
 		else if (prefix == "PHYS")
 		{
-			btVector3 Pos, Size;
+			float mass, fric;
+			glm::vec3 Pos, Size;
+			ss >> mass >> Pos.x >> Pos.y >> Pos.z >> Size.x >> Size.y >> Size.z >> fric;
+			btVector3 physPos = BtToGlm::GLMTOBTV3(Pos);
+			btVector3 physSize = BtToGlm::GLMTOBTV3(Size);
+
+			RenderingManager::activeScene->FindFirst(GOName).emplace<PhysicsBody>();
+			RenderingManager::activeScene->FindFirst(GOName).get<PhysicsBody>().AddBody(mass, physPos, physSize, fric);
+
 
 		}
 		
